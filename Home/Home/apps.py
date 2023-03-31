2e3a5ba34dbf04a67ef0ac4c1c41e69375cfb1a1
akshay sonawane <akshay.sonawane0239@gmail.com>
2023-03-31 16:13:33 +0530

    refreshed

---
+++ Home/Home/apps.py
@@ -0,0 +1,16 @@
+from django.apps import AppConfig
+
+
+class HomeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Home'
+
+
+    # icon_name = 'Home'
+class DanceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Dance'
+    # icon_name = 'Home' 
+
+
+
---
+++ cashfree_settings.py
@@ -0,0 +1,4 @@
+# Replace with your Cashfree credentials
+CASHFREE_APP_ID = 'your-app-id'
+CASHFREE_SECRET_KEY = 'your-secret-key'
+CASHFREE_ENV = 'test'  # Replace with 'prod' for production environment
